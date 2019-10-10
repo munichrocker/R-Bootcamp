91152e2a9c31dc16f286b3bfa35f0a66f76bf422
kira <kira.schacht@protonmail.com>
2019-10-10 09:26:25 +0200

    Block 3 initial commit

---
+++ Block 3 - Kira/scraping/uebung_djascrape.R
@@ -0,0 +1,64 @@
+#### Skript aufsetzen ####
+setwd("~/home/kira/Nextcloud/Journocode/Workshops/ifp/scraping")
+library(rvest);library(dplyr)
+
+
+#### Erste Seite testweise scrapen ####
+
+#Source Code Abschnitte mit Projektinfos holen
+tmp = read_html("https://datajournalismawards.org/projects/") %>%
+  html_nodes(".col-project__content")
+
+#Relevante Daten herausfiltern
+title =   tmp %>% html_nodes("h3 > a") %>% html_text()
+by =      tmp %>% html_nodes("p > span") %>% html_text() %>% gsub("Project by\n","",.) %>% trimws()
+country = tmp %>% html_nodes("p > em") %>% html_text()
+link =    tmp %>% html_nodes("h3 > a") %>% html_attr("href")
+excerpt = tmp %>% html_nodes(".project-excerpt") %>% html_text()
+
+#Zu Data Frame kombinieren
+dja = data.frame(title, by, country, link, excerpt, stringsAsFactors = F)
+
+#aufräumen
+rm(list = ls())
+
+
+#### Alle Seiten im loop scrapen ####
+
+# Leere Liste und Counter initialisieren
+n = 14; l = vector("list", length=n)
+
+# Durch alle 142 Seiten loopen
+for( i in 1:n ) {
+    #Statusanzeige in der Konsole
+    cat(i, "of", n, "\n")
+  
+    #Source Code Abschnitte mit Projektinfos holen
+    tmp = read_html(paste0("https://datajournalismawards.org/projects/page/",i)) %>% html_nodes(".col-project__content")
+    
+    #Relevante Daten herausfiltern
+    title =   tmp %>% html_nodes("h3>a") %>% html_text()
+    by =      tmp %>% html_nodes("p > span") %>% html_text() %>% gsub("Project by\n","",.) %>% trimws()
+    country = tmp %>% html_nodes("p > em") %>% html_text()
+    link =    tmp %>% html_nodes("h3>a") %>% html_attr("href")
+    excerpt = tmp %>% html_nodes(".project-excerpt") %>% html_text()
+    
+    #Zu Data Frame kombinieren
+    l[[i]] = data.frame(title, by, country, link, excerpt, stringsAsFactors = F)
+}
+#Aufräumen
+rm(i, n, tmp, title, by, country, link, excerpt)
+
+#Listenelemente zu einem großen data frame zusammenfassen
+dja = bind_rows(l)
+
+#Data cleaning: Autor und Organisation als einzelne Spalten speichern, Spaltenreihenfolge umsortieren
+dja = dja %>%
+    mutate(author = gsub("Project by |[\r\t\n]+ / .+$","", by),
+           organisation = gsub("^.+[\r\t\n]+ / Organisation: ","", by),
+           excerpt = gsub("[\r\t\n\\]*","", excerpt)) %>% 
+    select(title, author, organisation, country, link, excerpt)
+
+#Datensatz als CSV schreiben
+write.csv2(dja, "dja.csv", row.names = F, na = "")
+
