94a8648320586f7461d2e896000da04ae1f91b8d
Steffen Kühne <stekhn@gmail.com>
2019-11-04 11:56:54 +0100

    Add homework draft

---
+++ Block 4 - Steffen/homework_esc.R
@@ -0,0 +1,25 @@
+library(rvest);
+library(dplyr);
+
+# Datensatz scrapen: https://stekhn.de/esc/esc_winners.html
+
+# Datensatz in einen Vektor, Data-Frame oder Tibble umwandeln
+
+# Datensatz kennenlernen:
+## - Ist der Datensatz vollständig?
+## - Welche Anomalien gibt es?
+## - Was gibt es noch zu klären, bevor du mit der Analyse beginnst
+
+# Analysefragen
+## - Welche Länder haben am häufigsten gewonnen?
+## - Welches Land war am häufigsten Zweiter (“runner-up”)?
+## - Lieder mit welcher Sprache haben am häufigsten gewonnen?
+## - Wo fand der ESC am häufigsten statt?
+## - Bonus: Gibt es ein geografisches Muster?
+
+# Was könnte man noch analysieren?
+
+# Du bist heute verantwortlicher Redakteuer
+## - Was ist hier die erzählenswerte Geschichte?
+## - Wie würdest du die Geschichte (visuell?) aufbereiten
+
